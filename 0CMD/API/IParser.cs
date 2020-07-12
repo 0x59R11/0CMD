6f8a71ff13f22d93daa61fb6bed5f5444f91c87c
AuD1t3k <uriybulakh12@gmail.com>
2020-07-12 12:41:57 +0300

    Add project files.

---
+++ 0CMD/API/IParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0CMD.API
+{
+    public interface IParser
+    {
+        bool ParseBool(object obj);
+        
+
+        byte ParseByte(object obj);
+        sbyte ParseSByte(object obj);
+
+
+        short ParseShort(object obj);
+        ushort ParseUshort(object obj);
+
+
+        int ParseInt(object obj);
+        uint ParseUint(object obj);
+
+
+        long ParseLong(object obj);
+        ulong ParseUlong(object obj);
+
+
+        float ParseFloat(object obj);
+
+
+        double ParseDouble(object obj);
+
+
+        decimal ParseDecimal(object obj);
+
+
+        char ParseChar(object obj);
+        string ParseString(object obj);
+    }
+}
